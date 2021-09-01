354da2d4bfaefbb3f04edaabcb8576c0453f3c10
Bas Harenslak <bas@astronomer.io>
2021-09-01 12:04:26 +0200

    Don't load examples (in case you havent set AIRFLOW__CORE__LOAD_EXAMPLES=False)

--- tests/dags/test_dag_integrity.py
+++ tests/dags/test_dag_integrity.py
@@ -8,7 +8,7 @@ def test_dagbag():
     Validate DAG files using Airflow's DagBag.
     This includes sanity checks e.g. do tasks have required arguments, are DAG ids unique & do DAGs have no cycles.
     """
-    dag_bag = DagBag()
+    dag_bag = DagBag(include_examples=False)
     assert not dag_bag.import_errors  # Import errors aren't raised but captured to ensure all DAGs are parsed
 
     # Additional project-specific checks can be added here, e.g. to enforce each DAG has a tag
